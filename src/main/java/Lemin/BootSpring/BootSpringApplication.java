aa64d522797d2c3d6127ab477b07842abd1dcfd2
Lemin Ely <med2005lemin@gmail.com>
2026-04-18 12:45:14 +0000

    ini programme

---
+++ src/main/java/Lemin/BootSpring/BootSpringApplication.java
@@ -0,0 +1,13 @@
+package Lemin.BootSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringApplication.class, args);
+	}
+
+}
